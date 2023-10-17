9d96409b7cbc939c59e9b42d17f2e80e58949e99
Khalid Shawki <khalid.i.shawki@gmail.com>
2023-10-17 03:52:30 +0300

    Add: Footer

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -30,7 +30,6 @@ const GetApp = () => {
             />
           </div>
         </div>
-
         <div className='flex flex-1 items-center justify-end'>
           <Image src='/phones.png' alt='phones' width={550} height={870} />
         </div>
